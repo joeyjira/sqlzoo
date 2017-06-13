1172a97c7cd348f7869a1c118588e5e1fd09f9d5
joeyjira <joey.jira@gmail.com>
2017-06-12 17:34:24 -0700

    complete 08

--- lib/08_using_null.rb
+++ lib/08_using_null.rb
@@ -62,6 +62,11 @@ def teachers_and_mobiles
   # 444 2266' if no number is given. Show teacher name and mobile
   # #number or '07986 444 2266'
   execute(<<-SQL)
+    SELECT
+      teachers.name,
+      COALESCE(teachers.mobile, '07986 444 2266')
+    FROM
+      teachers
   SQL
 end
 
@@ -70,6 +75,13 @@ def teachers_and_depts
   # department name. Use the string 'None' where there is no
   # department.
   execute(<<-SQL)
+    SELECT
+      teachers.name,
+      COALESCE(depts.name, 'None')
+    FROM
+      teachers
+    LEFT JOIN
+      depts ON teachers.dept_id = depts.id
   SQL
 end
 
@@ -78,6 +90,11 @@ def num_teachers_and_mobiles
   # mobile phones.
   # NB: COUNT only counts non-NULL values.
   execute(<<-SQL)
+    SELECT
+      COUNT(teachers.name) AS teacher_count,
+      COUNT(teachers.mobile) AS moble_count
+    FROM
+      teachers
   SQL
 end
 
@@ -86,6 +103,16 @@ def dept_staff_counts
   # the number of staff. Structure your JOIN to ensure that the
   # Engineering department is listed.
   execute(<<-SQL)
+    SELECT
+      depts.name,
+      COUNT(teachers.name)
+    FROM
+      depts
+    LEFT JOIN
+      teachers ON depts.id = teachers.dept_id
+    GROUP BY
+      depts.name
+
   SQL
 end
 
@@ -93,6 +120,16 @@ def teachers_and_divisions
   # Use CASE to show the name of each teacher followed by 'Sci' if
   # the the teacher is in dept 1 or 2 and 'Art' otherwise.
   execute(<<-SQL)
+    SELECT
+      teachers.name,
+      CASE
+        WHEN teachers.dept_id IN (1, 2) THEN
+          'Sci'
+        ELSE
+          'Art'
+      END
+    FROM
+      teachers
   SQL
 end
 
@@ -101,5 +138,17 @@ def teachers_and_divisions_two
   # the the teacher is in dept 1 or 2, 'Art' if the dept is 3, and
   # 'None' otherwise.
   execute(<<-SQL)
+    SELECT
+      teachers.name,
+      CASE
+        WHEN teachers.dept_id IN (1, 2) THEN
+          'Sci'
+        WHEN teachers.dept_id = 3 THEN
+          'Art'
+        ELSE
+          'None'
+      END
+    FROM
+      teachers
   SQL
 end
